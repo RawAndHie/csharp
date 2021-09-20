2e14218fb9f2a5b8601b9cb0724d6b0448996323
RawAndHie <ductath2008041@fpt.edu.vn>
2021-09-20 19:01:27 +0700

    commit

--- AppHelloWorld/MainPage.xaml.cs
+++ AppHelloWorld/MainPage.xaml.cs
@@ -26,7 +26,7 @@ namespace AppHelloWorld
         {
             this.InitializeComponent();
         }
-        //hello
+        //push 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
             MediaElement mediaElement = new MediaElement();
