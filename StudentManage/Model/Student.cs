dbc8558ac81984dbc379ba6c56dcf2206d43c41e
RawAndHie <ductath2008041@fpt.edu.vn>
2021-10-06 17:10:34 +0700

    ASM

--- StudentManage/Model/Student.cs
+++ StudentManage/Model/Student.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
-namespace U41.Model
+namespace StudentManage.Model
 {
-    public class Contact
+    public class Student
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public int Count { get; set; }
+        public string FullName { get; set; }
+        public string BirthDay { get; set; }
         public string Avatar { get; set; }
     }
+
     public class Avatar
     {
         public string AvatarImage { get; set; }
@@ -20,11 +23,20 @@ namespace U41.Model
     {
         public static List<Avatar> GetAvatars()
         {
-            
+
             var avatars = new List<Avatar>();
             avatars.Add(new Avatar { AvatarImage = "Assets/avatar.png" });
             avatars.Add(new Avatar { AvatarImage = "Assets/avatar2.png" });
             return avatars;
+        }
+    }
+
+    public class StudentManager
+    {
+        public static ObservableCollection<Student> GetListStudents()
+        {
+            var students = new ObservableCollection<Student>();
+            return students;
         }
     }
 }
