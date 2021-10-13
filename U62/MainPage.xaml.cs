77c2a341c4e16503fbf06db4aac51b6046424b66
RawAndHie <ductath2008041@fpt.edu.vn>
2021-10-13 16:00:13 +0700

    U62

--- U62/MainPage.xaml.cs
+++ U62/MainPage.xaml.cs
@@ -31,12 +31,15 @@ namespace U62
         private async void GetWeather_Click(object sender, RoutedEventArgs e)
         {
             var str = TextBoxCity.Text;
+            
             RootObject myWeather = await OpenWeatherMapProxy.GetWeather(str.Trim());
             CityName.Text = myWeather.name;
             ResultTextBlock.Text = 
                 ((int)myWeather.main.temp).ToString()
                 + " - "
                 + myWeather.weather[0].description;
+            string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
+            ImageWeather.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
